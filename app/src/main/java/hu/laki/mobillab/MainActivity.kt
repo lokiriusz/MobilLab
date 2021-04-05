8abc88a135bf3c74128750454a5582b0e68ce2ca
Laki Dávid <lakidavid97@gmail.com>
2021-04-05 13:24:23 +0200

    Init project

---
+++ app/src/main/java/hu/laki/mobillab/MainActivity.kt
@@ -0,0 +1,11 @@
+package hu.laki.mobillab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
