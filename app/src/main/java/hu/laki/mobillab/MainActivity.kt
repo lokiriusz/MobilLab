83b6854a11afa884156b61d0dda468ba88e11405
Laki Dávid <lakidavid97@gmail.com>
2021-04-14 21:16:50 +0200

    Add Toolbar title handling

--- app/src/main/java/hu/laki/mobillab/MainActivity.kt
+++ app/src/main/java/hu/laki/mobillab/MainActivity.kt
@@ -2,15 +2,21 @@ package hu.laki.mobillab
 
 import android.os.Bundle
 import co.zsmb.rainbowcake.navigation.SimpleNavActivity
+import hu.laki.mobillab.helpers.ToolbarHandler
 import hu.laki.mobillab.ui.jokes.JokesFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : SimpleNavActivity() {
+class MainActivity : SimpleNavActivity(), ToolbarHandler {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         if (savedInstanceState == null) {
             navigator.add(JokesFragment())
         }
+    }
+
+    override fun setToolbarTitle(title: String) {
+        toolbar.title = title
     }
 
 }
