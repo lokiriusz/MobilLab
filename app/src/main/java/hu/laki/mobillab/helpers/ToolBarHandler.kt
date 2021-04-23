7ab631723d6bc8e79b897ecd49a5a5507a8b2b76
Laki Dávid <47417721+lokiriusz@users.noreply.github.com>
2021-04-23 19:36:57 +0200

    Feature - About screen (#6)
    * Implement About screen

--- app/src/main/java/hu/laki/mobillab/helpers/ToolBarHandler.kt
+++ app/src/main/java/hu/laki/mobillab/helpers/ToolBarHandler.kt
@@ -1,11 +1,22 @@
 package hu.laki.mobillab.helpers
 
 import androidx.fragment.app.Fragment
+import co.zsmb.rainbowcake.navigation.navigator
+import com.google.android.material.appbar.MaterialToolbar
+import hu.laki.mobillab.R
 
 interface ToolbarHandler {
-    fun setToolbarTitle(title: String)
+    fun getToolbar(): MaterialToolbar
 }
 
-fun Fragment.setToolbarTitle(title: String) {
-    (activity as? ToolbarHandler)?.setToolbarTitle(title)
+fun Fragment.setToolbarTitle(title: String, pop: Boolean = false) {
+    (activity as? ToolbarHandler)?.getToolbar()?.setTitle(title)
+
+    if (pop) {
+        (activity as? ToolbarHandler)?.getToolbar()?.setNavigationIcon(R.drawable.ic_arrow_back)
+        (activity as? ToolbarHandler)?.getToolbar()?.setNavigationOnClickListener { navigator?.pop() }
+    } else {
+        (activity as? ToolbarHandler)?.getToolbar()?.setNavigationIcon(null)
+    }
 }
+
