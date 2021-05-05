b6d64ad8c55d4dd0274ac2626e95fdeb3ab83d44
Laki Dávid <lakidavid97@gmail.com>
2021-05-05 12:21:17 +0200

    Add FavouritesViewModel tests

--- app/src/test/java/hu/laki/mobillab/test/ViewModelTest.kt
+++ app/src/test/java/hu/laki/mobillab/test/ViewModelTest.kt
@@ -4,6 +4,11 @@ import co.zsmb.rainbowcake.test.assertObserved
 import co.zsmb.rainbowcake.test.observeStateAndEvents
 import hu.laki.mobillab.domain.model.Joke
 import hu.laki.mobillab.mock.domain.mockJoke
+import hu.laki.mobillab.mock.domain.mockJokeWith
+import hu.laki.mobillab.ui.favourites.FavouritesLoading
+import hu.laki.mobillab.ui.favourites.FavouritesPresenter
+import hu.laki.mobillab.ui.favourites.FavouritesReady
+import hu.laki.mobillab.ui.favourites.FavouritesViewModel
 import hu.laki.mobillab.ui.jokes.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
@@ -19,7 +24,7 @@ import org.robolectric.RobolectricTestRunner
 class ViewModelTest {
 
     @Test
-    fun `test joke is retrieved on load`() = runBlockingTest {
+    fun `test joke is retrieved`() = runBlockingTest {
         val joke: Joke = mockJoke()
         val presenter: JokesPresenter = mock()
         presenter.stub {
@@ -57,7 +62,7 @@ class ViewModelTest {
     }
 
     @Test
-    fun `test joke is not retrieved on load`() = runBlockingTest {
+    fun `test joke is not retrieved`() = runBlockingTest {
         val joke: Joke? = null
         val presenter: JokesPresenter = mock()
         presenter.stub {
@@ -90,6 +95,62 @@ class ViewModelTest {
 
             stateObserver.assertObserved(
                     JokeReady(joke)
+            )
+        }
+    }
+
+
+    @Test
+    fun `test favourite jokes are retrieved`() = runBlockingTest {
+        val jokes: List<Joke> = listOf(
+                mockJokeWith("mockFavouriteJoke1"),
+                mockJokeWith("mockFavouriteJoke2"),
+                mockJokeWith("mockFavouriteJoke3")
+        )
+        val presenter: FavouritesPresenter = mock()
+        presenter.stub {
+            onBlocking { getFavourites() } doReturn jokes
+        }
+
+        val viewModel: FavouritesViewModel = FavouritesViewModel(presenter)
+        viewModel.observeStateAndEvents { stateObserver, _ ->
+            viewModel.loadFavourites()
+
+            stateObserver.assertObserved(
+                    FavouritesLoading,
+                    FavouritesReady(jokes)
+            )
+        }
+    }
+
+    @Test
+    fun `test a favourite joke is deleted`() = runBlockingTest {
+        val deletableJoke: Joke = mockJokeWith("mockFavouriteJoke1")
+        val presenter: FavouritesPresenter = mock()
+        presenter.stub {
+            onBlocking { getFavourites() } doReturn listOf(
+                    deletableJoke,
+                    mockJokeWith("mockFavouriteJoke2"),
+                    mockJokeWith("mockFavouriteJoke3")
+            )
+            onBlocking { deleteFavouriteJoke(deletableJoke) } doReturn listOf(
+                    mockJokeWith("mockFavouriteJoke2"),
+                    mockJokeWith("mockFavouriteJoke3")
+            )
+        }
+
+        val viewModel: FavouritesViewModel = FavouritesViewModel(presenter)
+        viewModel.observeStateAndEvents { stateObserver, _ ->
+            viewModel.deleteFavouriteJoke(mockJokeWith("mockFavouriteJoke1"))
+
+            stateObserver.assertObserved(
+                    FavouritesLoading,
+                    FavouritesReady(
+                            listOf(
+                                    mockJokeWith("mockFavouriteJoke2"),
+                                    mockJokeWith("mockFavouriteJoke3")
+                            )
+                    )
             )
         }
     }
